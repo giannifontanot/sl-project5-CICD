3ba99d400b6245fcc35df873b7ad454aa72c1577
Gianni Fontanot <giannifontanot@gmail.com>
2022-08-18 20:49:59 -0500

    New order added to the list of orders.

--- src/main/java/com/bootcamp/project5/aws/OrderDAO.java
+++ src/main/java/com/bootcamp/project5/aws/OrderDAO.java
@@ -7,19 +7,18 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 
-
 @Repository
 public class OrderDAO {
     public List<Order> getOrders() {
 
         return Stream.of(
-                new Order(101,"Monitor", 3, 250),
+                new Order(101, "Monitor", 3, 250),
                 new Order(102, "Mouse", 1, 20),
-                new Order( 103, "Keyboard",1, 150 ),
-                new Order(  104, "Computer", 1, 1200),
-                new Order(  105 , "HDMI cable", 3, 25 ),
-                new Order(  106 , "Microcontroller", 1, 125 )
-                new Order(  107 , "Computer case", 1, 180 )
+                new Order(103, "Keyboard", 1, 150),
+                new Order(104, "Computer", 1, 1200),
+                new Order(105, "HDMI cable", 3, 25),
+                new Order(106, "Microcontroller", 1, 125),
+                new Order(107, "Computer case", 1, 180)
         ).collect(Collectors.toList());
     }
 }
